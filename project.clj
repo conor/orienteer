c1702faa9b33f6c9c03236d012d4e05f3c166eca
Conor Wade <conorwade@gmail.com>
2015-03-27 16:31:08 +0000

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject orienteer "0.1.0-SNAPSHOT"
+  :description "Orienteer is a simple Clojure library for geolocating objects"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
